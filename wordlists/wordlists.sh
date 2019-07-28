54517d967060fd6c37666409f4c55dd53fdb72e1
Iheb_Hamad(pr0xy) <30606487+iheb0100@users.noreply.github.com>
2019-07-28 11:57:30 +0200

    Create wordlists.sh

---
+++ wordlists/wordlists.sh
@@ -0,0 +1,6 @@
+#---------------------------------------------------------
+# Seclists
+git clone https://github.com/danielmiessler/SecLists
+# Werdlists
+git clone https://github.com/decal/werdlists
+#---------------------------------------------------------
