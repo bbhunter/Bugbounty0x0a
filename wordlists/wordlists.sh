fc0e1ec1532e50258d0493a095700f65ec5cf969
Iheb_Hamad(pr0xy) <30606487+iheb0100@users.noreply.github.com>
2019-07-28 12:02:48 +0200

    Update wordlists.sh

--- wordlists/wordlists.sh
+++ wordlists/wordlists.sh
@@ -3,4 +3,18 @@
 git clone https://github.com/danielmiessler/SecLists
 # Werdlists
 git clone https://github.com/decal/werdlists
+# Probable-Wordlists
+git clone https://github.com/berzerk0/Probable-Wordlists
+#
+git clone 
+#
+git clone 
+#
+git clone 
+#
+git clone 
+#
+git clone 
+#
+git clone 
 #---------------------------------------------------------
