0bd747d42424eb470069cede2b4a818e4edc394f
Iheb_Hamad(pr0xy) <30606487+iheb0100@users.noreply.github.com>
2019-07-28 11:52:35 +0200

    Update Subdomains_tools.sh

--- Tools/Subdomains_tools.sh
+++ Tools/Subdomains_tools.sh
@@ -1,10 +1,28 @@
+#---------------------------------------------------------
 # Sublist3r Tool 
 git clone https://github.com/aboul3la/Sublist3r
 # Dnscan tool
-git clone 
+git clone https://github.com/rbsec/dnscan
 # Massdns tool
-git clone 
+git clone https://github.com/blechschmidt/massdns
 # Knockpy Tool
+git clone https://github.com/guelfoweb/knock
+#
 git clone 
-
+#
 git clone 
+#
+git clone 
+#
+git clone 
+#
+git clone 
+#
+git clone 
+#
+git clone 
+#
+git clone 
+#
+git clone 
+#-----------------------------------------------------------
